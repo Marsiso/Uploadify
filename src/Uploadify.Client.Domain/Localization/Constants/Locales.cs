82676a8f03d81173917a3d8be4cef51c72eccf62
Marek Olšák <olsak.marek@outlook.cz>
2024-01-22 18:07:54 +0100

    UPLOADIFY-5 - Added localization and resources.

---
+++ src/Uploadify.Client.Domain/Localization/Constants/Locales.cs
@@ -0,0 +1,8 @@
+﻿namespace Uploadify.Client.Domain.Localization.Constants;
+
+public static class Locales
+{
+    public const string Default = English;
+    public const string English = "en";
+    public const string Czech = "cs";
+}
